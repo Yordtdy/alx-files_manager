c7ee9b1b4045cc37b49b2bf7c8f128c2fc0e0206
yordtdy <jordantarsh@gmail.com>
2024-08-07 12:54:46 -0700

    commit

--- worker.js
+++ worker.js
@@ -12,11 +12,13 @@ const fileQueue = new Queue('thumbnail generation');
 const userQueue = new Queue('email sending');
 
 /**
+ * this code works as: 
  * Generates the thumbnail of an image with a given width size.
  * @param {String} filePath The location of the original file.
  * @param {number} size The width of the thumbnail.
  * @returns {Promise<void>}
  */
+
 const generateThumbnail = async (filePath, size) => {
   const buffer = await imgThumbnail(filePath, { width: size });
   console.log(`Generating file: ${filePath}, size: ${size}`);
